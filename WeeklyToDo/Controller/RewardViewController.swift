2eae1788b63e09f5d4ab9c1c791f97328d5eab57
hinakko <131275914+hinakko02@users.noreply.github.com>
2023-08-27 12:00:00 +0900

    View Reward

--- WeeklyToDo/Controller/RewardViewController.swift
+++ WeeklyToDo/Controller/RewardViewController.swift
@@ -6,17 +6,35 @@
 //
 
 import UIKit
+import RealmSwift
 import XLPagerTabStrip
 
-class RewardViewController: UIViewController {
-
+class RewardViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     //ここがボタンのタイトルに利用されます
     var itemInfo: IndicatorInfo = "ごほうび"
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var toDoItems: Results<Item>?
+    let realm = try! Realm()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.rowHeight = 60.0
+        tableView.register(UINib(nibName: "ToDoListTableViewCell", bundle: nil), forCellReuseIdentifier: "Cell")
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoItems?.count ?? 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ToDoListTableViewCell
+        cell.checkImageView.image = UIImage(systemName: "square")
+
+        cell.toDoLabel.text = "ごほうびを記入してください"
+        return cell
+    }
 }
 
 extension RewardViewController: IndicatorInfoProvider {
