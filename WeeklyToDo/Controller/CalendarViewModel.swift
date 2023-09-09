3ac12ce82cecabaf57ad3f34f9e5ebb3aed6a8d2
Kenichi Tatsuhama <kenichi.tatsuhama@gmail.com>
2023-09-09 18:06:17 +0900

    リファクタリング（途中）

---
+++ WeeklyToDo/Controller/CalendarViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  CalendarViewModel.swift
+//  WeeklyToDo
+//
+//  Created by 辰濱健一 on 2023/09/09.
+//
+
+import Foundation
+
+class CalendarViewModel {
+    let result = [];
+    func filterReadRealm(calendarDay:String) {
+        result = [];
+    }
+    
+    func getResult()->[Conent]{
+        return [];
+    }
+    
+    func getCount() -> int {
+        return 0;
+    }
+}
