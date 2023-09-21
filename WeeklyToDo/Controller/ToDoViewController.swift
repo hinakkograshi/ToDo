4c90d4ab51bf2b80dea42b5197db8028693176f4
hinakko <131275914+hinakko02@users.noreply.github.com>
2023-09-22 00:04:17 +0900

    fix: cellの大きさを整えた

--- WeeklyToDo/Controller/ToDoViewController.swift
+++ WeeklyToDo/Controller/ToDoViewController.swift
@@ -31,6 +31,7 @@ class ToDoViewController: UIViewController, UITableViewDelegate, UITableViewData
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.rowHeight = 60.0
         tableView.register(UINib(nibName: "ToDoListTableViewCell", bundle: nil), forCellReuseIdentifier: "Cell")
         setDismissKeyboard()
         //自動でレビュー画面表示
